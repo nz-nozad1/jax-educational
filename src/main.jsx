61dd019a816bd3de8b2637ce50f489cc647154e3
nz-nozad1 <nz.nozad7@gmail.com>
2026-09-09 08:27:26 +0300

    Add runtime error diagnostics

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+window.onerror=(m,s,l,c,e)=>{document.body.innerHTML="<pre style="color:red;white-space:pre-wrap;padding:20px">"+m+"\n"+(e?.stack||"")+"</pre>";};
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
